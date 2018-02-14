c3b4a705cdcfb7104cf8ee48f414854fb8359b2e
Sasa Bolic <sasa.bolic@sixhours.io>
2018-02-14 15:41:12 +0100

    Create netty server

---
+++ src/main/java/io/sixhours/rx/NettyServer.java
@@ -0,0 +1,45 @@
+package io.sixhours.rx;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class NettyServer {
+
+    private int port;
+
+    public NettyServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        final int port = args.length > 0 ? Integer.parseInt(args[0]) : 8080;
+
+        new NettyServer(port).run();
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            new ServerBootstrap()
+                    .option(ChannelOption.SO_BACKLOG, 1_024)
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .bind(this.port)
+                    .sync()
+                    .channel()
+                    .closeFuture()
+                    .sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
