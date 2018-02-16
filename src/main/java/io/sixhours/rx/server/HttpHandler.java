ce4d11ca13ad8a819d0156caaf80bb5c35c6ab49
Sasa Bolic <sasa.bolic@sixhours.io>
2018-02-16 12:20:20 +0100

    Release request after it has been used

--- src/main/java/io/sixhours/rx/server/HttpHandler.java
+++ src/main/java/io/sixhours/rx/server/HttpHandler.java
@@ -11,12 +11,11 @@ import io.netty.util.AsciiString;
 import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
 import static io.netty.handler.codec.http.HttpHeaderNames.LOCATION;
 import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.CONTINUE;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 @ChannelHandler.Sharable
 class HttpHandler extends ChannelInboundHandlerAdapter {
-
-    private RequestHandler handler = new RequestHandler();
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
@@ -28,28 +27,41 @@ class HttpHandler extends ChannelInboundHandlerAdapter {
 
         if (msg instanceof FullHttpRequest) {
             FullHttpRequest request = (FullHttpRequest) msg;
+            try {
 
-            final Response res = handler.handle(ctx, msg);
-
-            final FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.valueOf(res.getStatusCode()), res.getBody() != null ? Unpooled.wrappedBuffer(res.getBody()) : Unpooled.buffer(0));
-
-            res.getHeaders().forEach((k, v) -> {
-                if (k.equalsIgnoreCase(LOCATION.toString())) {
-                    v = "http://" + request.headers().get(HttpHeaderNames.HOST, "unknown") + request.uri() + v;
+                if (HttpUtil.is100ContinueExpected(request)) {
+                    send100Continue(ctx);
                 }
-                response.headers().set(k, new AsciiString(v));
-            });
 
-            boolean keepAlive = HttpUtil.isKeepAlive(request);
+                final Response res = RequestHandler.handle(ctx, msg);
 
-            if (!keepAlive) {
-                ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-            } else {
-                response.headers().set(CONNECTION, KEEP_ALIVE);
-                ctx.write(response);
+                final FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.valueOf(res.getStatusCode()), res.getBody() != null ? Unpooled.wrappedBuffer(res.getBody()) : Unpooled.buffer(0));
+
+                res.getHeaders().forEach((k, v) -> {
+                    if (k.equalsIgnoreCase(LOCATION.toString())) {
+                        v = "http://" + request.headers().get(HttpHeaderNames.HOST, "unknown") + request.uri() + v;
+                    }
+                    response.headers().set(k, new AsciiString(v));
+                });
+
+                boolean keepAlive = HttpUtil.isKeepAlive(request);
+
+                if (!keepAlive) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            } finally {
+                request.release();
             }
         }
 
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, CONTINUE);
+        ctx.write(response);
     }
 
     @Override
